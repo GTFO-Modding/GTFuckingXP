628b94711890c2a9d9e62016fa5b2d539d63294f
Dinorush <onparnorthstar@gmail.com>
2024-12-01 14:37:04 -0500

    Version bump

--- GTF_Xp/BepInExLoader.cs
+++ GTF_Xp/BepInExLoader.cs
@@ -27,7 +27,7 @@ namespace GTFuckingXP
         MODNAME = "GTFuckingXP",
         AUTHOR = "Endskill",
         GUID = AUTHOR + "." + MODNAME,
-        VERSION = "2.3.0";
+        VERSION = "2.3.1";
 
         public static bool RundownDevMode { get; private set; }
         public static ConfigEntry<bool> DebugMessages { get; private set; }

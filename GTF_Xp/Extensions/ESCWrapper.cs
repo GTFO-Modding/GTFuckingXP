fc38e058f8b02e0a4d7fca794bc0363a08e29e72
Dinorush <onparnorthstar@gmail.com>
2024-12-01 14:36:19 -0500

    Add ESC speed support
    ExtraSyringeCustomization has speed-modifying syringed. This lets ESC handle movement speed calculations if it exists.

---
+++ GTF_Xp/Extensions/ESCWrapper.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx.Unity.IL2CPP;
+using ExtraSyringeCustomization.Utils;
+using System.Runtime.CompilerServices;
+
+namespace GTFuckingXP.Extensions
+{
+    internal class ESCWrapper
+    {
+        public const string PLUGIN_GUID = "dev.flaff.gtfo.ExtraSyringeCustomization";
+        public static bool HasESC { get; }
+
+        static ESCWrapper()
+        {
+            HasESC = IL2CPPChainloader.Instance.Plugins.ContainsKey(PLUGIN_GUID);
+        }
+
+        public static void Init()
+        {
+            if (HasESC)
+                AddRefreshCallbackESC();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void AddRefreshCallbackESC() => MovementMultiplierManager.AddRefreshCallback(OnRefresh);
+        private static float OnRefresh() => CacheApiWrapper.GetActiveLevel().CustomScaling.FirstOrDefault(buff => buff.CustomBuff == Enums.CustomScaling.MovementSpeedMultiplier)?.Value ?? 1f;
+
+        public static bool RefreshMovementSpeed()
+        {
+            if (!HasESC) return false;
+
+            RefreshMovementSpeedESC();
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void RefreshMovementSpeedESC() => MovementMultiplierManager.Refresh();
+    }
+}
